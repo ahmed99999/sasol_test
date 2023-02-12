a96d48ac1c208391dd37ed5228f6ae5cff0f755b
Ahmed Amin Bakhabou <Ahmedaminhd@outlook.com>
2023-02-12 21:12:25 +0100

    8- adding getTempPrice method in Product Model

--- sasol_backend/sasol_api/models.py
+++ sasol_backend/sasol_api/models.py
@@ -1,5 +1,6 @@
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
+import datetime
 
 
 class Product(models.Model):
@@ -12,3 +13,13 @@ class Product(models.Model):
     tags = ArrayField(models.CharField(max_length=255))
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def getTempPrice(self):
+        start = datetime.time(10, 0, 0)
+        end = datetime.time(15, 00, 0)
+        current = datetime.datetime.now().time()
+        return self.price if start <= current <= end else self.getDiscountedPrice()
+
+    def getDiscountedPrice(self):
+        price = float(self.price.replace(',', '')) * 10 / 100
+        return str(format(price, '.2f'))
