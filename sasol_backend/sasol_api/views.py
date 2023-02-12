68c0ad22d2b3f18eaf4814afabc30c0f9c3493a2
Ahmed Amin Bakhabou <Ahmedaminhd@outlook.com>
2023-02-12 21:13:53 +0100

    10- adding ProductDetail  view and routes

--- sasol_backend/sasol_api/views.py
+++ sasol_backend/sasol_api/views.py
@@ -1,6 +1,10 @@
+from django.http import Http404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import Product
 from .serializers import ProductSerializer
-from rest_framework import filters
+from rest_framework import filters, status
 from rest_framework import generics
 
 
@@ -9,3 +13,30 @@ class ProductList(generics.ListAPIView):
     serializer_class = ProductSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ['name']
+
+
+class ProductDetail(APIView):
+    # noinspection PyMethodMayBeStatic
+    def get_object(self, pk):
+        try:
+            return Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        snippet = self.get_object(pk)
+        serializer = ProductSerializer(snippet)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        snippet = self.get_object(pk)
+        serializer = ProductSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        snippet = self.get_object(pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
