fefc5207c9f8fee369246fec9ef222e77a418087
Ahmed Amin Bakhabou <Ahmedaminhd@outlook.com>
2023-02-12 21:13:24 +0100

    9- adding getTempPrice method in ProductSerializer

--- sasol_backend/sasol_api/serializers.py
+++ sasol_backend/sasol_api/serializers.py
@@ -3,6 +3,12 @@ from .models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    temp_price = serializers.SerializerMethodField()
+
+    # noinspection PyMethodMayBeStatic
+    def get_temp_price(self, instance):
+        return instance.getTempPrice()
+
     class Meta:
         db_table = 'products'
         model = Product
