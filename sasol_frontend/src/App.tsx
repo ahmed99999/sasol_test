fef5a28744b94312acaa8a9c058915316438f642
Ahmed Amin Bakhabou <Ahmedaminhd@outlook.com>
2023-02-14 12:57:49 +0100

    26- adding basic routing to the app

--- sasol_frontend/src/App.tsx
+++ sasol_frontend/src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import ProductsPage from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
 
 function App() {
   return (
     <div className="App">
-        React APP
+      <Routes>
+        <Route path="products" element={<ProductsPage />} />
+        <Route path="product/:productId" element={<ProductDetail />} />
+        <Route path="*" element={<Navigate to="/products" replace />} />
+      </Routes>
     </div>
   );
 }
---
+++ sasol_frontend/src/pages/ProductDetail.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductDetail = () => {
+  return <div>products</div>;
+};
+
+export default ProductDetail;
