dcf237d5e5050d6543887fbadd8551bf86c3a915
Ahmed Amin Bakhabou <Ahmedaminhd@outlook.com>
2023-02-14 17:51:00 +0100

    29- refactoring fetchProducts service

--- sasol_frontend/src/services/products.ts
+++ sasol_frontend/src/services/products.ts
@@ -1,14 +1,15 @@
 import request from "../lib/request";
 import { ProductResponse } from "../models";
 
-const getFetchURL = (searchValue: string) =>
-  "/products" + searchValue ? "?search=searchValue" : "";
+const getProductsFetchURL = (searchValue: string) => {
+  return "/products" + (searchValue ? "?search=" + searchValue : "");
+};
 
 const fetchProducts = async (
   searchValue: string = ""
 ): Promise<ProductResponse[]> => {
   try {
-    const url = getFetchURL(searchValue);
+    const url = getProductsFetchURL(searchValue);
     const response = await request.get<ProductResponse[]>(url);
 
     return response.data;
