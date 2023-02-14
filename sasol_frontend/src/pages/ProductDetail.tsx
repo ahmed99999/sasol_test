fbfd28bcf38ef93532f2bf27e625a46a34fa3e5e
Ahmed Amin Bakhabou <Ahmedaminhd@outlook.com>
2023-02-14 18:37:52 +0100

    38- initial ProductDetail.tsx component

--- sasol_frontend/src/pages/ProductDetail.tsx
+++ sasol_frontend/src/pages/ProductDetail.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useProduct } from "../hooks/useProduct";
 
 const ProductDetail = () => {
-  return <div>products</div>;
+  const { productId = "" } = useParams();
+  const { isError, isLoading, data: product } = useProduct(parseInt(productId));
+
+  if (isLoading) {
+    return <div>Loading Product...</div>;
+  }
+
+  if (isError) {
+    return <div>Error Loading Product, please try again later</div>;
+  }
+
+  if (!product) {
+    return <div>this product was not found</div>;
+  }
+
+  return (
+    <div>
+      <div>{product.name}</div>
+      <div>{product.price}</div>
+      <div>{product.active}</div>
+      <div>{product.description}</div>
+    </div>
+  );
 };
 
 export default ProductDetail;
