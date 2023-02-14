8d48b33121c33a61183af75e00f4cac82397b4fe
Ahmed Amin Bakhabou <Ahmedaminhd@outlook.com>
2023-02-14 17:49:00 +0100

    27- allowing cors origin requests

--- sasol_backend/sasol_backend/settings.py
+++ sasol_backend/sasol_backend/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
 
     "rest_framework",
     "django_filters",
@@ -49,9 +50,15 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "sasol_backend.urls"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
 
 TEMPLATES = [
     {
