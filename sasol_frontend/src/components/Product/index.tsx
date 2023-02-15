9a63e13634e0c871248377c4ba0fde2498dbb613
Ahmed Amin Bakhabou <Ahmedaminhd@outlook.com>
2023-02-15 21:37:22 +0100

    50- re-implementing Product component

--- sasol_frontend/src/components/Product/index.tsx
+++ sasol_frontend/src/components/Product/index.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 import { ProductResponse } from "../../models";
+import styles from "./index.module.scss";
+import { Link } from "react-router-dom";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 interface Props {
   product: ProductResponse;
@@ -7,9 +15,29 @@ interface Props {
 
 const Product = ({ product }: Props) => {
   return (
-    <div>
-      <div>{product.name}</div>
-    </div>
+    <Card sx={{ width: 200 }}>
+      <CardMedia
+        sx={{ height: 150 }}
+        image={product.picture}
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {product.name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          <b>Price: </b>
+          {product.price}$
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">
+          <Link className={styles.textLink} to={`/products/${product.id}`}>
+            Learn More
+          </Link>
+        </Button>
+      </CardActions>
+    </Card>
   );
 };
 
